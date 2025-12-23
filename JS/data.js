275bc9544d3ebf4910cf53edd8fbc0f36b08fdae
mohomozo <mohamadmozafari95@gmail.com>
2025-12-23 23:06:02 +0330

    Create data.js

---
+++ JS/data.js
@@ -0,0 +1,70 @@
+const myProjects = [
+    // --- ANIMATION ---
+    {
+        category: 'animation',
+        title: 'MOHOMOZO - Animation Reel',
+        dimensions: 'FHD',
+        material: 'Digital Animation / Mixed Media',
+        concept: 'مجموعه‌ای از تجربیات متحرک‌سازی و کاوش در فرم‌های بصری متغیر.',
+        image: 'https://vumbnail.com/689181790.jpg', // پیش‌نمایش خودکار از ویمئو
+        videoUrl: 'https://player.vimeo.com/video/689181790'
+    },
+    {
+        category: 'animation',
+        title: 'Character Study',
+        dimensions: 'FHD',
+        material: '2D Animation',
+        concept: 'بررسی حرکات و آناتومی در فضای دیجیتال.',
+        image: 'https://vumbnail.com/799850813.jpg',
+        videoUrl: 'https://player.vimeo.com/video/799850813'
+    },
+    {
+        category: 'animation',
+        title: 'Experimental Motion',
+        dimensions: 'FHD',
+        material: 'Experimental',
+        concept: 'ترکیب تکنیک‌های مختلف برای ایجاد حس تعلیق در حرکت.',
+        image: 'https://vumbnail.com/911468451.jpg',
+        videoUrl: 'https://player.vimeo.com/video/911468451'
+    },
+
+    // --- VIDEO ---
+    {
+        category: 'video',
+        title: 'Visual Journey I',
+        dimensions: '4K',
+        material: 'Video Art',
+        concept: 'کاوشی در تصاویر انتزاعی و محیطی.',
+        image: 'https://vumbnail.com/906983541.jpg',
+        videoUrl: 'https://player.vimeo.com/video/906983541'
+    },
+    {
+        category: 'video',
+        title: 'Visual Journey II',
+        dimensions: '4K',
+        material: 'Video Art',
+        concept: 'ادامه پروژه‌های ویدیو آرت با تمرکز بر نور و سایه.',
+        image: 'https://vumbnail.com/906984919.jpg',
+        videoUrl: 'https://player.vimeo.com/video/906984919'
+    },
+    {
+        category: 'video',
+        title: 'Performance Video',
+        dimensions: 'FHD',
+        material: 'Multimedia',
+        concept: 'ثبت و بازنمایی ویدئویی از یک اجرای هنری.',
+        image: 'https://vumbnail.com/906986112.jpg',
+        videoUrl: 'https://player.vimeo.com/video/906986112'
+    },
+
+    // --- WORK IN PROGRESS (W.I.P) ---
+    {
+        category: 'work',
+        title: 'Ongoing Project 2024',
+        dimensions: 'Developing',
+        material: 'Mixed Media',
+        concept: 'این پروژه در حال حاضر در مرحله توسعه قرار دارد و نسخه‌های اولیه آن در حال تدوین است.',
+        image: 'https://vumbnail.com/906988251.jpg',
+        videoUrl: 'https://player.vimeo.com/video/906988251'
+    }
+];
